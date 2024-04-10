e4425e57b656290fc2cdd1247e334c56c8265190
BazookaLegend <magaletabutterfly@gmail.com>
2024-04-10 15:29:16 +0200

    icon

--- dashboard/includes/header.php
+++ dashboard/includes/header.php
@@ -58,7 +58,7 @@ require_once "classes/user.php";
         </div>
         <ul class="navbar-nav navbar-right">
           <li class="dropdown"><a href="#" data-toggle="dropdown"
-              class="nav-link dropdown-toggle nav-link-lg nav-link-user"> <img alt="image" src="assets/img/user.png"
+              class="nav-link dropdown-toggle nav-link-lg nav-link-user"> <img alt="image" src="assets/img/user.jpg"
                 class="user-img-radious-style"> <span class="d-sm-none d-lg-inline-block"></span></a>
             <div class="dropdown-menu dropdown-menu-right pullDown">
               <div class="dropdown-title">Hello <?= $_SESSION['login_first_name'];?> <?= $_SESSION['login_last_name'];?></div>
